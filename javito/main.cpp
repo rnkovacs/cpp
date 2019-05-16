48c73cb6f15191b5575d3ad8a5b7b7711a687e14
Réka Kovács <rekanikolett@gmail.com>
2019-05-16 11:29:03 +0200

    Create main.cpp

---
+++ javito/main.cpp
@@ -0,0 +1,19 @@
+#include <cassert>
+#include <iostream>
+
+#include "array_heap.hpp"
+
+int main() {
+  ArrayOnHeap a(32, 2);
+  assert(a.size() == 2);
+  assert(a.get()[0] == 32);
+  assert(a.get()[1] == 32);
+
+  ArrayOnHeap b(745, 2);
+  ArrayOnHeap c(-13, 6);
+  ArrayOnHeap d = c;
+  b = a;
+
+  assert(d.get() != c.get());
+  assert(b.get() != a.get());
+}
